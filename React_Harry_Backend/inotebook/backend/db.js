82d6a7b71856c0a526092614ef6812b35204390e
Ayesha Deshwali <139254189+AyeshaDeshwali@users.noreply.github.com>
2024-09-27 18:28:44 +0530

    Delete...

--- React_Harry_Backend/inotebook/backend/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017/harry";
-const connenctToMongo = async () => {
-  await mongoose.connect(mongoURI);
-};
-
-module.exports = connenctToMongo;
