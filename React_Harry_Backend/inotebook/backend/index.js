ba059ca21276cc197b2ad3fc8af7da1cbdf1e437
ayeshaDeshwali <ayeshadeshwali@gmail.com>
2024-09-27 18:22:59 +0530

    add files

---
+++ React_Harry_Backend/inotebook/backend/index.js
@@ -0,0 +1,16 @@
+let connenctToMongo = require("./db");
+connenctToMongo();
+const express = require("express");
+const app = express();
+const port = 5000;
+app.use(express.json());
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!!!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
