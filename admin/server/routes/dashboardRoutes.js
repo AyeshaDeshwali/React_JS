47bda578b9d9c4dd82f6ceb2d6c18dd0e83f67c0
ayeshaDeshwali <ayeshadeshwali@gmail.com>
2024-11-17 13:50:07 +0530

    add file...

--- admin/server/routes/dashboardRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getDashboardData } = require("../controllers/dashboardController");
-
-router.get("/", getDashboardData);
-
-module.exports = router;
