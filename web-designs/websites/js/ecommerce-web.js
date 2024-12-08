327da7d5e39ed393ac24d8fe4c93b32573e8242d
ayeshaDeshwali <ayeshadeshwali@gmail.com>
2024-12-08 23:46:18 +0530

    add files

---
+++ web-designs/websites/js/ecommerce-web.js
@@ -0,0 +1,82 @@
+function copyMenu() {
+  let dptCategory = document.querySelector(".dpt-cat");
+  let dptPlace = document.querySelector(".deparments");
+  dptPlace.innerHTML = dptCategory.innerHTML;
+
+  let mainNav = document.querySelector(".header-nav nav");
+  let navPlace = document.querySelector(".off-convas nav");
+  navPlace.innerHTML = mainNav.innerHTML;
+
+  let topNav = document.querySelector(".header-top .wrapper");
+  let topPlace = document.querySelector(".off-convas .thetop-nav");
+  topPlace.innerHTML = topNav.innerHTML;
+}
+copyMenu();
+
+let MenuButton = document.querySelector(".trigger");
+let CloseButton = document.querySelector(".t-close");
+let addClass = document.querySelector(".site");
+
+MenuButton.addEventListener("click", function () {
+  addClass.classList.toggle("showmenu");
+});
+
+CloseButton.addEventListener("click", function () {
+  addClass.classList.remove("showmenu");
+});
+
+const SubMenu = document.querySelectorAll(".has-child .icon-samll");
+SubMenu.forEach((menu) => menu.addEventListener("click", toggle));
+
+function toggle(e) {
+  e.preventDefault();
+  SubMenu.forEach((item) =>
+    item != this ? item.closest(".has-child").classList.remove("expend") : null
+  );
+  if (this.closest(".has-child").classList != "expend")
+    this.closest(".has-child").classList.toggle("expend");
+}
+
+// slider --------------------------
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+// show search---------------------------
+
+const serchButton = document.querySelector(".t-search");
+tClose = document.querySelector(".search-close");
+showClose = document.querySelector(".site");
+serchButton.addEventListener("click", function () {
+  showClose.classList.toggle("showsearch");
+});
+tClose.addEventListener("click", function () {
+  showClose.classList.remove("showsearch");
+});
+
+// scrollToTopBtn-------------------
+function smoothScrollToTop() {
+  const scrollStep = -window.scrollY / (500 / 500);
+  const scrollInterval = setInterval(() => {
+    if (window.scrollY !== 0) {
+      window.scrollBy(0, scrollStep);
+    } else {
+      clearInterval(scrollInterval);
+    }
+  }, 500);
+}
+window.onscroll = function () {
+  const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 150
+  ) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+};
